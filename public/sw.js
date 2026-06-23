dc5b3d124b4dc2a2e0d03eb742aece1c1507427b
Peter <mlandulapeter7@gmail.com>
2026-06-23 02:26:37 -0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,29 @@
+const CACHE = "marketplace-v1";
+const ASSETS = ["/", "/marketplace", "/manifest.json"];
 
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {}));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone)).catch(() => {});
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
